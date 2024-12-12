fd7abb2ac0bd8e16f2c9f0b315f6de083206b382
Rumcajs121 <z.miara45@gmail.com>
2024-12-12 13:05:11 +0100

    Finished Task1.

--- Studia_Semestr5_Programistyczne/ConcurrencyProgramming/Zad1/CP_Zad1/Program.cs
+++ Studia_Semestr5_Programistyczne/ConcurrencyProgramming/Zad1/CP_Zad1/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Worker worker = new Worker();
+Thread[] threads = new Thread[10];
+for (int i = 0; i < threads.Length; i++)
+{
+    int threadId = i + 1;
+    threads[i] = new Thread(() => worker.CountWorker(threadId));
+    threads[i].Start();
+}
 
-Console.WriteLine("Hello, World!");
+
+foreach (Thread thread in threads)
+{
+    thread.Join();
+}
+
+public class Worker()
+{
+
+    public void CountWorker(int threadId)
+    {
+        for (int i = 0; i < 10; i++)
+        { 
+            int rnd = new Random().Next(1,10000);
+            Console.WriteLine("Worker thread {0}: Random number: {1}",threadId, rnd);
+            Thread.Sleep(100);
+        }
+    }
+}
